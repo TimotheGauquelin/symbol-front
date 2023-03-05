af2b71d61eb33ae88ecbc93af4762fb162e0a1be
Timothé <timothegauquelin@gmail.com>
2023-03-05 21:38:04 +0100

    [FEAT][TG] - Add Axios package & create getRandomApology() function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import api_base_url from "../api/api_base_url";
 
 const Home = () => {
-  return <div className="App-header">Hello Symbol-IT</div>;
+  const [apology, setApology] = useState({});
+  const [refresh, setRefresh] = useState(false);
+
+  const getRandomApology = () => {
+    axios
+      .get(`http://localhost:8080/api/public/apologies/random`)
+      .then((response) => {
+        if (response.status === 200) {
+          setApology(response.data);
+          setRefresh(false);
+        }
+      });
+  };
+
+  useEffect(() => {
+    getRandomApology();
+  }, [refresh]);
+
+  return (
+    <div className="App-header">
+      <h1 className="">Dev's Apologies</h1>
+      {refresh ? "LOADING" : <p>{apology.message}</p>}
+      <button
+        className="bg-gray-100 rounded text-sm text-black font-bold m-2 py-2 px-5 hover:bg-gray-300 cursor-pointer shadow-lg"
+        onClick={() => {
+          setRefresh(true);
+        }}
+      >
+        Generate a New Apology
+      </button>
+    </div>
+  );
 };
 
 export default Home;
