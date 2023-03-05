a448cb97e139d0d8faa3f3245626dadaff19bb67
Timothé <timothegauquelin@gmail.com>
2023-03-05 22:19:43 +0100

    [REFACTO][TG] - send getRandomApology() function from Home Page to Message Component to refresh only Message Component instead of all Home Page and its components

--- src/components/generic/Message.jsx
+++ src/components/generic/Message.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api_base from "../../api/api_base";
+import { URL_BACK_GET_RANDOMLY_APOLOGY } from "../../constants/urlsBack";
 import Loader from "./Loader";
 
-const Message = ({ refresh, apology }) => {
+const Message = ({ refresh, setRefresh }) => {
+  const [apology, setApology] = useState({});
+
+  const getRandomApology = () => {
+    api_base.get(URL_BACK_GET_RANDOMLY_APOLOGY).then((response) => {
+      if (response.status === 200) {
+        setApology(response.data);
+        setRefresh(false);
+      }
+    });
+  };
+
+  useEffect(() => {
+    getRandomApology();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [refresh]);
+
   return <div>{refresh ? <Loader /> : <p>{apology.message}</p>}</div>;
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,14 @@
-import React, { useEffect, useState } from "react";
-import api_base from "../api/api_base";
+import React, { useState } from "react";
 import Button from "../components/generic/Button";
-import Loader from "../components/generic/Loader";
 import Message from "../components/generic/Message";
-import { URL_BACK_GET_RANDOMLY_APOLOGY } from "../constants/urlsBack";
 
 const Home = () => {
-  const [apology, setApology] = useState({});
   const [refresh, setRefresh] = useState(false);
-
-  const getRandomApology = () => {
-    api_base.get(URL_BACK_GET_RANDOMLY_APOLOGY).then((response) => {
-      if (response.status === 200) {
-        setApology(response.data);
-        setRefresh(false);
-      }
-    });
-  };
-
-  useEffect(() => {
-    getRandomApology();
-  }, [refresh]);
 
   return (
     <div className="App-header">
       <h1 className="">Dev's Apologies</h1>
-      <Message refresh={refresh} apology={apology} />
+      <Message refresh={refresh} setRefresh={setRefresh} />
       <Button
         textButton="Generate Random Apology"
         action={() => {
