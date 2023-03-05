c9ddd5f4b484ea0ffb4a0cb80cc6aff28821e524
Timothé <timothegauquelin@gmail.com>
2023-03-05 22:11:42 +0100

    [REFACTO][TG] - Refacto Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,21 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
-import api_base_url from "../api/api_base_url";
+import api_base from "../api/api_base";
+import Button from "../components/generic/Button";
+import Loader from "../components/generic/Loader";
+import Message from "../components/generic/Message";
+import { URL_BACK_GET_RANDOMLY_APOLOGY } from "../constants/urlsBack";
 
 const Home = () => {
   const [apology, setApology] = useState({});
   const [refresh, setRefresh] = useState(false);
 
   const getRandomApology = () => {
-    axios
-      .get(`http://localhost:8080/api/public/apologies/random`)
-      .then((response) => {
-        if (response.status === 200) {
-          setApology(response.data);
-          setRefresh(false);
-        }
-      });
+    api_base.get(URL_BACK_GET_RANDOMLY_APOLOGY).then((response) => {
+      if (response.status === 200) {
+        setApology(response.data);
+        setRefresh(false);
+      }
+    });
   };
 
   useEffect(() => {
@@ -24,15 +25,13 @@ const Home = () => {
   return (
     <div className="App-header">
       <h1 className="">Dev's Apologies</h1>
-      {refresh ? "LOADING" : <p>{apology.message}</p>}
-      <button
-        className="bg-gray-100 rounded text-sm text-black font-bold m-2 py-2 px-5 hover:bg-gray-300 cursor-pointer shadow-lg"
-        onClick={() => {
+      <Message refresh={refresh} apology={apology} />
+      <Button
+        textButton="Generate Random Apology"
+        action={() => {
           setRefresh(true);
         }}
-      >
-        Generate a New Apology
-      </button>
+      />
     </div>
   );
 };
