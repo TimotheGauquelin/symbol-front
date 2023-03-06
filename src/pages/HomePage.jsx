13704c00bbd3b14a39549bf53165c3b44a15c870
Timothé <timothegauquelin@gmail.com>
2023-03-06 10:34:49 +0100

    [REFACTO][TG] - Move getRandomApology call from ApologyMessage Component to HomePage to get ApologyMessage Component more generic. Now AologyMessage Component take a function inside getApology props to retrieve any apology

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,31 @@
 import React, { useState } from "react";
 import Button from "../components/generic/Button";
-import Message from "../components/generic/Message";
+import ApologyMessage from "../components/generic/ApologyMessage";
+import api_base from "../api/api_base";
+import { URL_BACK_GET_RANDOMLY_APOLOGY } from "../constants/urlsBack";
 
 const HomePage = () => {
   const [refresh, setRefresh] = useState(false);
 
+  const [apology, setApology] = useState({});
+
+  const getRandomApology = () => {
+    api_base.get(URL_BACK_GET_RANDOMLY_APOLOGY).then((response) => {
+      if (response.status === 200) {
+        setApology(response.data);
+        setRefresh(false);
+      }
+    });
+  };
+
   return (
     <div className="app_bg">
       <h1 className="">Dev's Apologies</h1>
-      <Message refresh={refresh} setRefresh={setRefresh} />
+      <ApologyMessage
+        refresh={refresh}
+        apology={apology}
+        getApology={() => getRandomApology()}
+      />
       <Button
         textButton="Generate Random Apology"
         action={() => {
