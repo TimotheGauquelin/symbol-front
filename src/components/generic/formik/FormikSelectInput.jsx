5e02f9ef48242ccfa13b3650dc6dd0b832d237cb
Timothé <timothegauquelin@gmail.com>
2023-03-06 14:24:09 +0100

    [FEAT][TG] - Create a Formik Select Input to retrieve data from select and push it inside Formik Object values

---
+++ src/components/generic/formik/FormikSelectInput.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const FormikSelectInput = ({
+  dataArray,
+  form: { setFieldValue },
+  field: { name, value },
+}) => {
+  return (
+    <div className="mt-2">
+      <select
+        className="text-black p-2 bg-gray-100 rounded"
+        id="input-form"
+        value={value?.label}
+        onChange={(e) => {
+          setFieldValue(name, { label: e.target.value });
+        }}
+      >
+        {dataArray &&
+          dataArray.map((op, index) => (
+            <option key={index} value={op.label}>
+              {op.label}
+            </option>
+          ))}
+      </select>
+    </div>
+  );
+};
+
+export default FormikSelectInput;
