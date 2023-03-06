0966fccd0bf4301dc7b7203b65af3894a4190e82
Timothé <timothegauquelin@gmail.com>
2023-03-06 14:06:06 +0100

    [FEAT][TG] - Add Formik package & Create a form to add a new apology

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,10 +3,12 @@ import Button from "../components/generic/Button";
 import ApologyMessage from "../components/generic/ApologyMessage";
 import api_base from "../api/api_base";
 import { URL_BACK_GET_ALL_APOLOGY } from "../constants/urlsBack";
+import { Field, Formik, Form } from "formik";
 
 const HomePage = () => {
   const [refresh, setRefresh] = useState(false);
   const [apology, setApology] = useState({});
+  const [modal, setModal] = useState(false);
 
   const getRandomApology = () => {
     api_base.get(URL_BACK_GET_ALL_APOLOGY).then((response) => {
@@ -27,7 +29,7 @@ const HomePage = () => {
   };
 
   return (
-    <div className="app_bg">
+    <div className="app_bg relative">
       <h1 className="">Dev's Apologies</h1>
       <ApologyMessage
         refresh={refresh}
@@ -41,6 +43,41 @@ const HomePage = () => {
           setRefresh(true);
         }}
       />
+      <button
+        className="bg-blue-300 p-3 rounded text-md"
+        onClick={() => setModal(true)}
+      >
+        New
+      </button>
+      {modal && (
+        <div className="absolute w-full h-full flex justify-center items-center">
+          <div className="bg-white h-1/2 w-1/2 rounded p-2">
+            <p className="text-black">Ajouter un composant :</p>
+            <Formik
+              initialValues={{
+                id: null,
+                message: "",
+              }}
+              onSubmit={(values) => console.log(values)}
+            >
+              <Form id="form" className="">
+                <div className="flex flex-col">
+                  <label className="mt-2 font-medium text-black">
+                    Message:{" "}
+                  </label>
+                  <Field
+                    placeholder="Write a message.."
+                    className="mt-2 p-2 bg-gray-100 text-black"
+                    type="text"
+                    name="message"
+                  />
+                </div>
+                <button className="bg-blue-300 p-2 rounded mt-3">Submit</button>
+              </Form>
+            </Formik>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
