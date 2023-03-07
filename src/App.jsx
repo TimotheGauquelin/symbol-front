abe30005644a1667b8f848aa5bd6183aff85b5b7
Timothé <timothegauquelin@gmail.com>
2023-03-07 14:51:52 +0100

    [REFACTO][TG] - Add new paths to urlsFront file and use them inside AppRoute & Add react-toastify package to create toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import "./styles/animation.css";
+import "react-toastify/dist/ReactToastify.css";
 import ApologyPage from "./pages/ApologyPage";
 import ErrorPage from "./pages/ErrorPage";
 import HomePage from "./pages/HomePage";
 import LostPage from "./pages/LostPage";
+import {
+  URL_FRONT_ERROR_PAGE,
+  URL_FRONT_HOME_PAGE,
+  URL_FRONT_LOST_PAGE,
+} from "./constants/urlsFront";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/lost" element={<LostPage />} />
-        <Route path="*" element={<ErrorPage />} />
+        <Route path={URL_FRONT_HOME_PAGE} element={<HomePage />} />
+        <Route path={URL_FRONT_LOST_PAGE} element={<LostPage />} />
+        <Route path={URL_FRONT_ERROR_PAGE} element={<ErrorPage />} />
         <Route path="/:http_code" element={<ApologyPage />} />
       </Routes>
     </BrowserRouter>
--- src/constants/urlsFront.js
+++ src/constants/urlsFront.js
@@ -1,2 +1,4 @@
 export const URL_FRONT_HOME_PAGE = `/`;
+export const URL_FRONT_LOST_PAGE = `/lost`;
+export const URL_FRONT_ERROR_PAGE = `*`;
 export const URL_FRONT_APOLOGY_PAGE = (httpCode) => `/${httpCode}`;
